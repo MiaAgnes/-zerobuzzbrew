05b524269ce2b6e3af239adc931844f5a2197aad
ceciliefrydendal <cillefj21@gmail.com>
2025-03-05 19:06:33 +0100

    js til leveringsmugligheder

--- js/kasse.js
+++ js/kasse.js
@@ -1,3 +1,36 @@
+
+
+
+
+
+// gør så når man trykker på checkboxen hjem-chek viser det eller fjener det mugligheder for hvor du kan hente tingene. 
+document.getElementById('hjem-chekbox').addEventListener('change', function() {
+    const hjemSections = [
+        'dao1-section',
+        'dao2-section',
+        'gls-section',
+        'post-section'
+    ];
+
+    hjemSections.forEach(sectionId => {
+        document.getElementById(sectionId).style.display = this.checked ? 'block' : 'none';
+    });
+});
+
+// Initial state
+const hjemSections = [
+    'dao1-section',
+    'dao2-section',
+    'gls-section',
+    'post-section'
+];
+
+hjemSections.forEach(sectionId => {
+    document.getElementById(sectionId).style.display = document.getElementById('hjem-chekbox').checked ? 'block' : 'none';
+});
+
+
+// gør så når man trykker på checkboxen hent-chek viser det eller fjener det mugligheder for hvor du kan hente tingene. 
 document.getElementById('hent-chekbox').addEventListener('change', function() {
     const hentSections = [
         'dao1-hent-section',
@@ -21,3 +54,7 @@ const hentSections = [
 
 hentSections.forEach(sectionId => {
     document.getElementById(sectionId).style.display = document.getElementById('hent-chekbox').checked ? 'block' : 'none';
+});
+
+
+
