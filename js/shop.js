3bd1e448b2aef2b56091aa21fcf252d56f8109da
MieZilmer <158352685+MieZilmer@users.noreply.github.com>
2025-03-06 17:30:55 +0100

    Læs mere sider

---
+++ js/shop.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+   document.querySelectorAll('.produkt-card').forEach(function(card) {
+     card.addEventListener('click', function(event) {
+       if (!event.target.closest('.add-to-cart')) {
+         window.location.href = card.getAttribute('data-url');
+       }
+     });
+   });
+ });
